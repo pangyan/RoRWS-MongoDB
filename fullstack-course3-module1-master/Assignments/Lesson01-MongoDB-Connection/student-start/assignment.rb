e4b4ea3e78417bd2d797170fce101087877cebfb
Israel Wong <israel_wong@calypso.com>
2017-11-28 00:13:31 +0800

    20171128 0013

--- fullstack-course3-module1-master/Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
+++ fullstack-course3-module1-master/Assignments/Lesson01-MongoDB-Connection/student-start/assignment.rb
@@ -13,20 +13,22 @@ class Solution
     #assign the client to @@db class variable and return that client
 	db = Mongo::Client.new('mongodb://localhost:27017')
 	db = db.use('test')
+	@@db = db
     return db
   end
 
   #Implement a class method in the `Solution` class called `collection` that will
   def self.collection
     #return the `zips` collection
-    
+	collection = @@db[:zips]
+	return collection
   end
 
   #Implement an instance method in the `Solution` class called `sample` that will
   def sample
     #return a single document from the `zips` collection from the database. 
     #This does not have to be random. It can be first, last, or any other document in the collection.
-
+	return @@db[:zips].find().first
   end
 end
 
